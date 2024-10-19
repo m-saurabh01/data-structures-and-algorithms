38bbef9a3fa3642a2d9eaa1876bc10a1c7ceedfb
eternal-harsh <msaurabh737@gmail.com>
2024-10-19 20:40:01 +0530

    Added problem in Bit manipulation section

---
+++ BitManipulation/KthBitInNthBinaryString.java
@@ -0,0 +1,56 @@
+package BitManipulation;
+
+
+/*				Leetcode 1545. Find Kth Bit in Nth Binary String Medium
+ * 
+ * 
+Given two positive integers n and k, the binary string Sn is formed as follows:
+
+S1 = "0"
+Si = Si - 1 + "1" + reverse(invert(Si - 1)) for i > 1
+Where + denotes the concatenation operation, reverse(x) returns the reversed string x, and invert(x) inverts all the bits in x (0 changes to 1 and 1 changes to 0).
+
+For example, the first four strings in the above sequence are:
+
+S1 = "0"
+S2 = "011"
+S3 = "0111001"
+S4 = "011100110110001"
+Return the kth bit in Sn. It is guaranteed that k is valid for the given n.
+
+ 
+
+Example 1:
+
+Input: n = 3, k = 1
+Output: "0"
+Explanation: S3 is "0111001".
+The 1st bit is "0".
+Example 2:
+
+Input: n = 4, k = 11
+Output: "1"
+Explanation: S4 is "011100110110001".
+The 11th bit is "1".
+*/
+public class KthBitInNthBinaryString {
+	
+	public char findKthBit(int n, int k) {
+
+        if(n==1)return '0';
+
+        int len= (1 << n)-1;
+
+        if(k<Math.ceil(len/2.0)){
+            return findKthBit(n-1,k);
+        }
+        else if(k==Math.ceil(len/2.0)){
+            return '1';
+        }else {
+            char ch=findKthBit(n-1,len-(k-1));
+            return (ch=='0')?'1':'0';
+        }
+        
+    }
+
+}
