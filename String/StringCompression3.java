7d930445de03a25ff2742f99b617d274c73a1a4c
eternal-harsh <msaurabh737@gmail.com>
2024-11-04 21:06:51 +0530

    Added problem in Strings section

---
+++ String/StringCompression3.java
@@ -0,0 +1,67 @@
+package String;
+
+/*	
+ * 				Leetcode 3163 String Compression 3 Medium
+ * 
+ * 
+Given a string word, compress it using the following algorithm:
+
+Begin with an empty string comp. While word is not empty, use the following operation:
+Remove a maximum length prefix of word made of a single character c repeating at most 9 times.
+Append the length of the prefix followed by c to comp.
+Return the string comp.
+
+ 
+
+Example 1:
+
+Input: word = "abcde"
+
+Output: "1a1b1c1d1e"
+
+Explanation:
+
+Initially, comp = "". Apply the operation 5 times, choosing "a", "b", "c", "d", and "e" as the prefix in each operation.
+
+For each prefix, append "1" followed by the character to comp.
+
+Example 2:
+
+Input: word = "aaaaaaaaaaaaaabb"
+
+Output: "9a5a2b"
+
+Explanation:
+
+Initially, comp = "". Apply the operation 3 times, choosing "aaaaaaaaa", "aaaaa", and "bb" as the prefix in each operation.
+
+For prefix "aaaaaaaaa", append "9" followed by "a" to comp.
+For prefix "aaaaa", append "5" followed by "a" to comp.
+For prefix "bb", append "2" followed by "b" to comp.
+
+*/ 
+public class StringCompression3 {
+	
+	public String compressedString(String word) {
+        StringBuilder comp=new StringBuilder();
+
+        int i=0;
+
+        while(i<word.length()){
+
+            char ch=word.charAt(i);
+            int num=1;
+            int j=i+1;
+            while(j<word.length() && num<9 && word.charAt(j)==ch){
+                num++;
+                j++;
+            }
+            comp.append(String.valueOf(num)).append(ch+"");
+            i=j;
+        }
+        
+
+        return comp.toString();
+    }
+
+}
