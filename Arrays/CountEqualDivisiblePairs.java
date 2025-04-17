05150ee2e2132ac47ed837bcbf3b0a7f668fbb6d
eternal-harsh <msaurabh737@gmail.com>
2025-04-17 19:20:29 +0530

    Added problem in Arrays section

---
+++ Arrays/CountEqualDivisiblePairs.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/*
+				2176. Count Equal and Divisible Pairs in an Array
+*/
+public class CountEqualDivisiblePairs {
+	
+	public int countPairs(int[] nums, int k) {
+	    int ans = 0;
+	    Map<Integer, List<Integer>> numToIndices = new HashMap<>();
+
+	    for (int i = 0; i < nums.length; ++i) {
+	      numToIndices.putIfAbsent(nums[i], new ArrayList<>());
+	      numToIndices.get(nums[i]).add(i);
+	    }
+
+	    for (List<Integer> indices : numToIndices.values()) {
+	      Map<Integer, Integer> gcds = new HashMap<>();
+	      for (final int i : indices) {
+	        final int gcd_i = gcd(i, k);
+	        for (final int gcd_j : gcds.keySet())
+	          if (gcd_i * gcd_j % k == 0)
+	            ans += gcds.get(gcd_j);
+	        gcds.merge(gcd_i, 1, Integer::sum);
+	      }
+	    }
+
+	    return ans;
+	  }
+
+	  private int gcd(int a, int b) {
+	    return b == 0 ? a : gcd(b, a % b);
+	  }
+
+}
