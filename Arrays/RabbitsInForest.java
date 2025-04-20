4791ffb34f2fb0091f07c7a7de275f23f97043a8
eternal-harsh <msaurabh737@gmail.com>
2025-04-20 22:07:12 +0530

    Added problem in Arrays section

---
+++ Arrays/RabbitsInForest.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+
+/*
+ * 
+ * 			781. Rabbits in Forest
+ * 
+*/
+public class RabbitsInForest {
+	
+	public int numRabbits(int[] answers) {
+	    int ans = 0;
+	    int[] count = new int[1000];
+
+	    for (final int answer : answers) {
+	      if (count[answer] % (answer + 1) == 0)
+	        ans += answer + 1;
+	      ++count[answer];
+	    }
+
+	    return ans;
+	  }
+
+}
