38e6fd584d3b9128cb3e44e8a8c50d20b367eee4
eternal-harsh <msaurabh737@gmail.com>
2025-07-28 21:23:51 +0530

    Added in Bit section

---
+++ BitManipulation/MaxBitwiseOR.java
@@ -0,0 +1,29 @@
+package BitManipulation;
+
+import java.util.Arrays;
+
+/*
+		2044. Count Number of Maximum Bitwise-OR Subsets
+*/
+public class MaxBitwiseOR {
+	
+	public int countMaxOrSubsets(int[] nums) {
+	    final int ors = Arrays.stream(nums).reduce((a, b) -> a | b).getAsInt();
+	    dfs(nums, 0, 0, ors);
+	    return ans;
+	  }
+
+	  private int ans = 0;
+
+	  private void dfs(int[] nums, int i, int path, final int ors) {
+	    if (i == nums.length) {
+	      if (path == ors)
+	        ++ans;
+	      return;
+	    }
+
+	    dfs(nums, i + 1, path, ors);
+	    dfs(nums, i + 1, path | nums[i], ors);
+	  }
+
+}
