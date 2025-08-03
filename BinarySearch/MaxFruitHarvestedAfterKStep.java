16e053d69515d61883ba9d83e75c2f033b79447f
eternal-harsh <msaurabh737@gmail.com>
2025-08-03 21:47:06 +0530

    Added in binary search

---
+++ BinarySearch/MaxFruitHarvestedAfterKStep.java
@@ -0,0 +1,43 @@
+package BinarySearch;
+
+/*
+		2106. Maximum Fruits Harvested After at Most K Steps
+*/
+public class MaxFruitHarvestedAfterKStep {
+	
+	public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+	    final int maxRight = Math.max(startPos, fruits[fruits.length - 1][0]);
+	    int ans = 0;
+	    int[] amounts = new int[1 + maxRight];
+	    int[] prefix = new int[2 + maxRight];
+
+	    for (int[] f : fruits)
+	      amounts[f[0]] = f[1];
+
+	    for (int i = 0; i + 1 < prefix.length; ++i)
+	      prefix[i + 1] = prefix[i] + amounts[i];
+
+	 
+	    final int maxRightSteps = Math.min(maxRight - startPos, k);
+	    for (int rightSteps = 0; rightSteps <= maxRightSteps; ++rightSteps) {
+	      final int leftSteps = Math.max(0, k - 2 * rightSteps); 
+	      ans = Math.max(ans, getFruits(startPos, maxRight, leftSteps, rightSteps, prefix));
+	    }
+
+	   
+	    final int maxLeftSteps = Math.min(startPos, k);
+	    for (int leftSteps = 0; leftSteps <= maxLeftSteps; ++leftSteps) {
+	      final int rightSteps = Math.max(0, k - 2 * leftSteps); 
+	      ans = Math.max(ans, getFruits(startPos, maxRight, leftSteps, rightSteps, prefix));
+	    }
+
+	    return ans;
+	  }
+
+	  private int getFruits(int startPos, int maxRight, int leftSteps, int rightSteps, int[] prefix) {
+	    final int l = Math.max(0, startPos - leftSteps);
+	    final int r = Math.min(maxRight, startPos + rightSteps);
+	    return prefix[r + 1] - prefix[l];
+	  }
+
+}
