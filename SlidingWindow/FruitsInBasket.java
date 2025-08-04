e98cfd2f21f35beff09312ab253755b22eb3f947
eternal-harsh <msaurabh737@gmail.com>
2025-08-04 21:35:02 +0530

    Added in sliding window

---
+++ SlidingWindow/FruitsInBasket.java
@@ -0,0 +1,28 @@
+package SlidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+		904. Fruit Into Baskets
+*/
+public class FruitsInBasket {
+	
+	public int totalFruit(int[] fruits) {
+	    int ans = 0;
+	    Map<Integer, Integer> count = new HashMap<>();
+
+	    for (int l = 0, r = 0; r < fruits.length; ++r) {
+	      count.merge(fruits[r], 1, Integer::sum);
+	      while (count.size() > 2) {
+	        count.merge(fruits[l], -1, Integer::sum);
+	        count.remove(fruits[l], 0);
+	        ++l;
+	      }
+	      ans = Math.max(ans, r - l + 1);
+	    }
+
+	    return ans;
+	  }
+
+}
